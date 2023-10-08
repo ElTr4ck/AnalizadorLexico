9a616af7b34432aa86b1de1c06dd02c8d607cf99
ElTr4ck <67998219+ElTr4ck@users.noreply.github.com>
2023-10-08 02:39:47 -0600

    No Python :(
    Se borra el intento fallido de Python

--- Nueva carpeta/token.py
+++
@@ -1,7 +0,0 @@
-class Token:
-    def __init__(self, tipo, lexema, literal=None):
-        self.tipo = tipo
-        self.lexema = lexema
-        self.literal = literal
-    def toString(self):
-        return f"< {self.tipo} {self.lexema} {self.literal} >"
